f480dd658acaf044563215bbdc00bddfb646f82f
OLASUPOPATRICKOLUSEYE <zachpascal969@gmail.com>
2025-06-06 18:54:54 +0100

    improve

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,17 @@
+
+
+const SingleTeacherPage = () => {
+    return (
+        <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+            {/* LEFT */}
+            <div className="w-full xl:2/3">
+            d
+            </div>
+
+            {/* RIGHT */}
+            <div className="w-full xl:1/3">r</div>
+        </div>
+    );
+};
+
+export default SingleTeacherPage;
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,10 +1,21 @@
 
-
-
 const Pagination = () => {
   return (
-    <div className="p-4 flex items-center justify-center text-gray-500">Pagination</div>
-  )
-}
+    <div className="p-4 px-1 flex items-center justify-between text-gray-500 ">
+      <button disabled className="px-4 py-2 bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Previous
+      </button>
+      <div className="flex items-center gap-1 text-sm">
+          <button className="px-2 rounded-sm bg-lamaSky">1</button>
+          <button className="px-2 rounded-sm">2</button>
+          <button className="px-2 rounded-sm">...</button>
+          <button className="px-2 rounded-sm">10</button>
+      </div>
+      <button disabled className="px-4 py-2 bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Next
+      </button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
